041c0ade5890bd83036653507a062865c69eea8b
Jimmy Ramos <jimmy.ramos@unitec.edu>
2017-08-18 13:01:25 -0600

    lib

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
-console.log(process.argv);
+#!/usr/bin/env node
+
+
+const chalk = require('chalk');
+const clear = require('clear');
+const figlet = require('figlet');
+const inquirer = require('inquirer');
+
+const files = require('./lib/files');
+
+clear();
+console.log(
+  chalk.yellow(
+    figlet.textSync('Ginit', { horizontalLayout: 'full' })
+  )
+);
+
+if (files.directoryExists('.git')) {
+  console.log(chalk.red('Already a git repository!'));
+  process.exit();
+}
+
+// function getGithubCredentials(callback) {
+const questions = [
+  {
+    name: 'username',
+    type: 'input',
+    message: 'Enter your Github username or e-mail address:',
+    validate(value) {
+      if (value.length) {
+        return true;
+      }
+      return 'Please enter your username or e-mail address';
+    }
+  },
+  {
+    name: 'password',
+    type: 'password',
+    message: 'Enter your password:',
+    validate(value) {
+      if (value.length) {
+        return true;
+      }
+      return 'Please enter your password';
+    }
+  }
+];
+
+inquirer.prompt(questions);
+// }
